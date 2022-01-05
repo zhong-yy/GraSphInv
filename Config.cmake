c81f71abc70f732015897f92ddf79a67bb8f5f16
Yiyuan Zhong <824549303@qq.com>
2022-01-05 10:17:31 +0800

    Add comments in Config.cmake

--- Config.cmake
+++ Config.cmake
@@ -1,3 +1,8 @@
+# Specify the c++ compiler. The default one is icpx (oneAPI).
 set(ENV{CXX} icpx)
+
+# Specify whether netcdf library will be used. The value should be TRUE or FALSE
 set(USE_NETCDF TRUE)
+
+# Specify whether mkl library will be used. THe value should be TRUE or FALSE
 set(USE_MKL TRUE)
